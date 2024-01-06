f5e1234c6efce6ab145ac0dc7fe512d0f43546c7
jf videos <72672581+Jfvideos1@users.noreply.github.com>
2024-01-06 15:12:39 -0300

    Update and rename m3u_maker.py to Jfvideos_oficial.m3u_maker.py

---
+++ Jfvideos_oficial.m3u_maker.py
@@ -0,0 +1,144 @@
+Jf Vídeos Oficial
+_______________________
+📽️Link canal Youtube
+👉💥https://youtube.com/c/JfvideosOficial
+________________________________________
+🌍Instagram
+👉💥 bit.ly/3i21WHT
+
+############################################################################ Canais | ABERTO
+
+
+#EXTM3U
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9w2W2cXcfnPSISAUjaoE87pEbj-3ZDflHMg&usqp=CAU"", TELECINE PIPOCA
+http://191.37.250.169:8801/live/TelecinePipocaHD/720p.m3u8
+
+#EXTINF:-1 tvg-logo ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9w2W2cXcfnPSISAUjaoE87pEbj-3ZDflHMg&usqp=CAU"", TELECINE PREMIUM
+http://191.37.250.169:8801/live/TelecinePremiumHD/720p.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9w2W2cXcfnPSISAUjaoE87pEbj-3ZDflHMg&usqp=CAU"",TELECINE ACTION
+http://64.31.40.14:14056/
+
+
+#EXTINF:-1 tvg-logo="https://play-lh.googleusercontent.com/fv2t4tFKJANqwXsaaG5EsjbOMS5zeX360nGmDASnuvqi6c-BdoNzTJMsNPS3TEJP6pE"", SONY CHANNEL
+http://199.127.60.83:16063/
+
+
+#EXTINF:-1 tvg-logo ="https://upload.wikimedia.org/wikipedia/commons/3/3e/Animal_Planet.png"", ANIMAL PLANET
+http://199.127.60.83:16192/
+
+
+#EXTINF:-1 tvg-logo="https://logodownload.org/wp-content/uploads/2018/03/history-channel-logo.png"",HISTORY
+http://84.17.44.179:14670/
+
+
+#EXTINF:-1 tvg-logo ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4lXajyAH_HQ8dtup6RQkh0LgiXZ-15F_iTw&usqp=CAU"", SERTÃO-TV 
+http://wz4.dnip.com.br/sertaotv/sertaotv.sdp/chunklist_w206800964.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Globo_logo_%282020-%29.svg/1428px-Globo_logo_%282020-%29.svg.png"", GLOBO-SP
+https://transcoder.proxy.tokplay.com.br/videos/REDEGLOBO/format720p_REDEGLOBO.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://logodownload.org/wp-content/uploads/2014/02/band-logo-tv-1.png"", BAND-RBAHD
+https://stmv1.paineltv.net/valeradiowebtv/valeradiowebtv/chunklist_w1796071075.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/pt/1/13/Logotipo_da_Rede_Record.png"", RECORD-SP HD
+https://cdn-1.nxplay.com.br/RECORD_PAULISTA_PFZ_NXPLAY_01/tracks-v1a1/mono.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://logodownload.org/wp-content/uploads/2013/12/sbt-logo-1.png"", SBT-PÍ HD
+https://stmv1.transmissaodigital.com/cidadeverdenovo/cidadeverdenovo/chunklist_w1725959751.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://logodownload.org/wp-content/uploads/2013/12/sbt-logo-1.png"", SBT-INTERIOR HD
+https://cdn.jmvstream.com/w/LVW-10801/LVW10801_Xvg4R0u57n/chunklist.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://logodownload.org/wp-content/uploads/2013/12/sbt-logo-1.png"", SBT-GO HD
+https://j5umwxziy3.zoeweb.tv/tvsd2/smil:tvsd2_64eS8OiMSj8l.smil/playlist.m3u8?sjwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQi
+
+
+#EXTINF:-1 tvg-logo="https://diariodorio.com/wp-content/uploads/2020/07/redetv-logo.png"", REDE TV-PIAUI
+https://59e4b6c1beddc.streamlock.net/tvodia/tvodia/chunklist_w225234320.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://soliens.com.br/wp-content/uploads/2019/02/record-news-1-e1549045735650.png"", RECOD NEWS
+https://5cf4a2c2512a2.streamlock.net/8016/8016/chunklist_w757204456.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjIXCTA443p5t1pcyK4v8th2zx360OARpY3w&usqp=CAU"",MEGAPIX
+http://64.31.40.14:14048/
+
+
+#EXTINF:-1 tvg-logo="https://seeklogo.com/images/H/hbo-logo-9BCB43E157-seeklogo.com.png"",HBO
+http://208.115.225.174:14136/
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/FX_Turkey_logo.svg/2560px-FX_Turkey_logo.svg.png"",FX
+http://64.31.40.14:14043/
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK8WHsqO7snxIJx4crhynuDGhMmIFyMEGaqw&usqp=CAU"", PREMIERE CLUBES 
+http://208.115.225.174:14460/
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK8WHsqO7snxIJx4crhynuDGhMmIFyMEGaqw&usqp=CAU"", PREMIERE 02
+http://208.115.225.174:14460/
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK8WHsqO7snxIJx4crhynuDGhMmIFyMEGaqw&usqp=CAU"", Premiere 03
+http://185.150.190.225:16468/
+
+
+#EXTINF:-1 tvg-logo="https://logodownload.org/wp-content/uploads/2017/07/sportv-logo-7-1.png"", SPORTV 01
+http://51.83.238.223:16437/
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/6/60/ESPN_logos.png"", ESPN
+http://51.83.238.223:16515/
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/e/ed/Canal-combate-logo.png"", COMBATE
+http://185.150.190.225:16493/
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFlTvXPYlvNCt7Jl4o8bYTCzbZAYRO8YByDw&usqp=CAU"", GLOBO NEWS
+http://209.222.97.92:16436/
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Canal_Viva_2018_wordmark.svg/1200px-Canal_Viva_2018_wordmark.svg.png"", CANAL VIVA 
+http://64.31.40.14:14884/
+
+
+#EXTINF:-1 tvg-logo="https://i.pinimg.com/originals/c3/0b/20/c30b207a7f70aada6d8fcf576db95fa1.png"", DRAGON BALL SUPER
+http://46.165.213.46:8080/Xplus-1-PmzYaD4Idm/2QNM6W3FGg/686?token=ShZcWRELFQxDXVQBAVULXVFWUlZbVgYHAwJTWFUHVQUJUQ0OVFJWDgJHThRCQktdVFhoWlwRDwMMBklDQkBRRW5dUBpYQQ4EVVFSDxMaG0xcWFIRAgICBQAFUFcECgYbE0RdXEBZFQBQVVMBExobXU1BUkFWUllqUFUTCFRWFg1fQVhUTkFYWD4BB1tQWF0aDxcHERQRXEZEEl8PQl9YGxNWXUwQAkNTQ19ABwUODBoZF1RcTV1BR00SX0N1YRYbE1FMTAcNRF8OC0AME0JKGhkXXkBnQVBGQEIAAFpWRhULFgQaTkFaVxk6AVlfWFxbQVxYXUsRDxcAEklDWlxaXkVbRmcSClMUW0dWBwkEDhpI
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC8LGestNL7HgWBC4D0skh0LxPhTek_pNzLg&usqp=CAU"", CARTOON NETWORK 
+https://playout.cdn.cartoonnetwork.com.br/playout_01/playlist-720p.m3u8?ManoTV
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNQzmWZd62p8scWMuDlOJUk-A6vel8cEsg2w&usqp=CAU"", GLOOB
+https://cdn-1.nxplay.com.br/AXN_TK/index.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://assets.stickpng.com/images/6132212900feb70004beb7c9.png"",  AXN
+http://top.x8full.com/PLAYPREMIUM/9Zf5EneSrS/3503.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Discovery_Science_2017_Logo.svg/1200px-Discovery_Science_2017_Logo.svg.png"",  DISCOVERY SCIENCI
+https://cdn-1.nxplay.com.br/DISCOVERY_SCIENCE_NX/index.m3u8
+#EXTINF:-1 tvg-logo="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7519b96c-71f8-4040-82d4-c80e18384f1b/dgfmxto-7bfed0f2-bf1e-4ac9-a816-c234c7abf621.png/v1/fill/w_774,h_509/hbo_xtreme_logo_concept_2023_by_wbblackofficial_dgfmxto-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTA5IiwicGF0aCI6IlwvZlwvNzUxOWI5NmMtNzFmOC00MDQwLTgyZDQtYzgwZTE4Mzg0ZjFiXC9kZ2ZteHRvLTdiZmVkMGYyLWJmMWUtNGFjOS1hODE2LWMyMzRjN2FiZjYyMS5wbmciLCJ3aWR0aCI6Ijw9Nzc0In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.jRJXSPScsMoK4t1sAQ2id3T5Y7PEScmx-BT0GqQNCBo"",  HBO EXTREME 
+http://top.x8full.com/PLAYPREMIUM/9Zf5EneSrS/2932.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_YX9UcDx3cGIeQsO9LOa4wBTesiPm0LdUuQ&usqp=CAU"",  HBO PLUS
+http://top.x8full.com/PLAYPREMIUM/9Zf5EneSrS/3503.m3u8
+
+
+#EXTINF:-1 tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM-6f6JdWljj6mw2mox372pU3ekiy_moPVnQ&usqp=CAU"",  STOR CHANNEL 
+http://ch2.cinefire.net/auth/r7yJJCSz-25MK3ko7JK2aK6fFroL81RHqkqz3NEtG0aOm2G5jqWd265V7hlrPMSqY29zuti0RXMQtR51GPm3P4A3Lpmsn-o205Z2fQGuX2NhwiZqc7PMMQwBZQX1nXBhqbP3uiUeLzy3RPFLUyC8lZc3guGorYuPiKTl4xmZv1tea6c--tqsgq4EhKUk0AMbs0wX-iBhLqdw88Bv8k1gUvqUOfJMaIUdd6TuQjpZILSUsYUemWrvj36GyjvJETk161tnje9zMnIrN6BNlqEmRF0yN1vSrRt3ZQNqgPpkMzlfVrhiTR6jDtFGsr1H_ZgqDLXLX-rxJYaL3Jm_SmkzxhqDl2vUvwhPrpKqZ_5Q4bw89lBFgUAXZa4P6Z9LTCWJe-jRnyPdzk_PbsAvpqFADdV_xDAgPmFtaB9ebCZwuKIfBa7pCFRZjuw2ssLfI-E61H81xjZtMf0CG-1ulWvSC2CYrcr-4aeejpu4U2PgDKifpu_conIGrvoSShOAb1CjQKFIFw2yckv0bguLNDqDwbg3HdiAsxSZLa4Eh7Vel5NY8CsYS5a6aM74ksH-ZSn-fdkuIVq8Bi-3q0KCwROGqMZKkUCUWMYSCyWQayeNX8jtJynj-fCjiy4CyOyA_Z-rJi5H0DcOZLUEfKXlJWfOv-KxDJLsqqGWFCGOdpx1GmI
+
